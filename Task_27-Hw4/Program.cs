7e6b8f4664e4b97d23b17b360f8a3ba4b541f1cc
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-08-20 08:00:29 +0300

    add added template Task 25 27 29

---
+++ Task_27-Hw4/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+Console.WriteLine("введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int i = A;
+for (A = факт A);{//ПРАВИЛЬНЕЕ для
+    }
+Console.WriteLine($"произвед");
+
+int sum = 1;
+if (A<1)
+{
+    Console.WriteLine($"НЕ возмо обра число");
+
+}
+else
+{
+    for(inti=1; i<=A; i++)
+}
+
+// адача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+int
+int[]arr = new int[8]//8 число эл в массиве
+new Random().Next()
+arr[0] =5;
+int n = array.Length;
+Console.WriteLine(arr.Length);
+//jkолег
+int
+int[]arr = new int[8]//олег 
+new Random().Next()
+arr[0] =5;
+for ()
+{
+    int n = array.Length;{
+    int A = new Random()
+    Console.WriteLine(array[i])
+
+Console.WriteLine(arr.Length);
---
+++ Task_29-Hw4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+Randow rand = new Randow();
+
+for (int i = 0; i< array.Length; i++)
+{
+    array[i] = rand.Next(0, 2); 
+    // Console.Write(array[i]+ "");
+}
+Console.WriteLine($[{string.Join(", ", array));
+Console.WriteLine($[{string.Join(", ", array)}]);
+Console.WriteLine($[{string.Join("/", array)}]);
+onsole.WriteLine($[{string.Join("f", array)}]);
